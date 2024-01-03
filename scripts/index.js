0b0ec63612511ce9190aee7b872fdd15b4bee8e1
aidanho1188 <aaron23657@gmail.com>
2024-01-03 04:31:05 -0500

    replace play with start game and made its button work with js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,9 @@
 // Do not change the import statement
 import placeholderQuestions from "./placeholder-questions.js";
-console.log({ placeholderQuestions });
+console.log({placeholderQuestions});
+
+const playbnt = document.getElementById("play");
+
+playbnt.addEventListener("click", () => {
+  window.location.href = "/round-1.html";
+});
